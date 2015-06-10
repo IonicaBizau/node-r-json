ed7e7aebd3769358bebc6f0b147670e2da84e8f4
Ionică Bizău <bizauionica@yahoo.com>
2015-06-10 16:51:14 +0300

    Comments

--- example/index.js
+++ example/index.js
@@ -1,7 +1,10 @@
+// Dependencies
 var ReadJson = require("../lib");
 
+// Read the json file asynchronously
 ReadJson(__dirname + "/test.json", function (err, data) {
     console.log(err || data);
 });
 
+// Read the same file synchronously
 console.log(ReadJson(__dirname + "/test.json"));
